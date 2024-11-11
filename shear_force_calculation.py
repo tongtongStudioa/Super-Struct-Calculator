aec6f27170f11b29760655f292c03c23c333e118
tongtongStudioa <>
2024-11-11 18:25:36 +0100

    modifs sur distributions cisaillements pour d'autres sections

--- shear_force_calculation.py
+++ shear_force_calculation.py
@@ -6,15 +6,23 @@ Created on Mon Oct 21 09:28:00 2024
 """
 
 import sys
-from PyQt5.QtWidgets import QApplication, QCheckBox, QWidget, QComboBox, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QFrame, QMessageBox
+from PyQt5.QtWidgets import (
+    QApplication, QCheckBox, QWidget, QComboBox, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QFrame, QMessageBox, QScrollArea
+    )
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
+
+PROFILE_H = "Profilé en H" 
+PROFILE_RECT = "Rectangulaire"
+PROFILE_T = "Profilé en T"
+ 
 # Classe principale pour l'application
 class ShearCalculator(QWidget):
+    
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -56,6 +64,25 @@ class ShearCalculator(QWidget):
                 padding: 20px;
                 border-radius: 8px;
             }
+            QComboBox {
+                background-color: #f0f0f0;
+                color: #333;
+                border-radius: 5px;
+                padding: 5px;
+                min-width: 120px;
+            }
+            QComboBox::down-arrow {
+                width: 12px;
+                height: 12px;
+            }
+            QComboBox QAbstractItemView {
+                background-color: #ffffff;
+                selection-background-color: #2874A6;
+                selection-color: #ffffff;
+                border: 1px solid #ddd;
+                border-radius: 5px;
+                padding: 5px;
+                }
         """)
 
         # Explication des hypothèses
@@ -72,9 +99,9 @@ class ShearCalculator(QWidget):
 
         # Menu déroulant pour choisir le profil de la poutre
         self.profile_combobox = QComboBox(self)
-        self.profile_combobox.addItem("Rectangulaire")
-        self.profile_combobox.addItem("Profil en I")
-        self.profile_combobox.addItem("Profil en T")
+        self.profile_combobox.addItems([PROFILE_RECT, PROFILE_H, PROFILE_T])
+        self.profile_type = self.profile_combobox.currentText()
+
 
         
         # Disposition des champs d'entrée et des labels
@@ -137,6 +164,7 @@ class ShearCalculator(QWidget):
         # Champs d'entrée pour les dimensions, changent en fonction du profilé
         self.input_forms_frame = QFrame(self)
         self.createProfileInputs()
+        
         # Ajouter la connexion avec l'adaptateur de vue une fois la valeur "input_forms_frame" créé
         self.profile_combobox.currentIndexChanged.connect(self.updateLayout)
         
@@ -159,7 +187,6 @@ class ShearCalculator(QWidget):
         vbox_input.addLayout(input_layout2)
         vbox_input.addLayout(input_layout3)
         
-        vbox_input = QVBoxLayout()
         vbox_input.addLayout(input_layout1)
         vbox_input.addWidget(self.auto_moment_checkbox)
         vbox_input.addLayout(input_layout4)
@@ -167,9 +194,12 @@ class ShearCalculator(QWidget):
         vbox_input.addWidget(self.input_forms_frame)
         #vbox_input.addLayout(input_layout5)
         #vbox_input.addLayout(input_layout6)
+        
         input_frame.setLayout(vbox_input)
-
-
+        # Ajouter input_frame dans un QScrollArea
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setWidget(input_frame)
 
         # Créer un widget de graphique avec matplotlib
         self.canvas = FigureCanvas(plt.Figure(figsize=(9, 6)))
@@ -190,7 +220,9 @@ class ShearCalculator(QWidget):
         main_layout.addWidget(hypothese_label)
         main_layout.addLayout(combo_layout)
         intermediate_layout = QHBoxLayout()
-        intermediate_layout.addWidget(input_frame)
+        # Ajouter la scroll_area à la main_layout
+        intermediate_layout.addWidget(self.scroll_area)
+        #intermediate_layout.addWidget(input_frame)
         intermediate_layout.addWidget(self.canvas)
         main_layout.addLayout(intermediate_layout)
         main_layout.addWidget(calculate_button, alignment=Qt.AlignCenter)
@@ -202,42 +234,45 @@ class ShearCalculator(QWidget):
         """Crée les champs d'entrée pour les dimensions en fonction du profilé."""
        
         self.clearLayout(self.input_forms_frame.layout())
-        layout = QVBoxLayout()
-
-        profile_type = self.profile_combobox.currentText()
+        if self.input_forms_frame.layout() is  None:
+            layout = QVBoxLayout()
+            self.input_forms_frame.setLayout(layout)
         
         # Dimensions pour un profilé rectangulaire
-        if profile_type == "Rectangulaire":
-            self.entry_hauteur = self.createInputField("Hauteur (mm):", layout)
-            self.entry_largeur = self.createInputField("Largeur (mm):", layout)
+        if self.profile_type == PROFILE_RECT :
+            self.entry_hauteur = self.createInputField("Hauteur (mm):", 600)
+            self.entry_largeur = self.createInputField("Largeur (mm)", 250)
         
         # Dimensions pour un profilé en H
-        elif profile_type == "Profilé en H":
-            self.entry_hauteur_totale = self.createInputField("Hauteur totale (mm):", layout)
-            self.entry_largeur_totale = self.createInputField("Largeur totale (mm):", layout)
-            self.entry_hauteur_ame = self.createInputField("Hauteur de l'âme (mm):", layout)
-            self.entry_epaisseur_ame = self.createInputField("Épaisseur de l'âme (mm):", layout)
-            self.entry_largeur_ailes = self.createInputField("Largeur des ailes (mm):", layout)
-            self.entry_epaisseur_ailes = self.createInputField("Épaisseur des ailes (mm):", layout)
+        elif self.profile_type == PROFILE_H :
+            self.entry_hauteur = self.createInputField("Hauteur totale (mm):", 600)
+            self.entry_largeur = self.createInputField("Largeur totale (mm):", 250)
+            self.entry_hauteur_ame = self.createInputField("Hauteur de l'âme (mm)", 500)
+            self.entry_epaisseur_ame = self.createInputField("Épaisseur de l'âme (mm):", 50)
+            self.entry_largeur_ailes = self.createInputField("Largeur des ailes (mm)", 250)
+            self.entry_epaisseur_ailes = self.createInputField("Épaisseur des ailes (mm):", 50)
         
         # Dimensions pour un profilé en T
-        elif profile_type == "Profilé en T":
-            self.entry_hauteur_totale = self.createInputField("Hauteur totale (mm):", layout)
-            self.entry_largeur_ame = self.createInputField("Largeur de l'âme (mm):", layout)
-            self.entry_epaisseur_ame = self.createInputField("Épaisseur de l'âme (mm):", layout)
-            self.entry_largeur_ailes = self.createInputField("Largeur de l'aile (mm):", layout)
-            self.entry_epaisseur_ailes = self.createInputField("Épaisseur de l'aile (mm):", layout)
+        elif self.profile_type == PROFILE_T :
+           self.entry_hauteur = self.createInputField("Hauteur totale (mm):", 600)
+           self.entry_largeur = self.createInputField("Largeur totale (mm):", 250)
+           self.entry_hauteur_ame = self.createInputField("Hauteur de l'âme (mm):", 500)
+           self.entry_epaisseur_ame = self.createInputField("Épaisseur de l'âme (mm):", 50)
+           self.entry_largeur_ailes = self.createInputField("Largeur des ailes (mm):", 250)
+           self.entry_epaisseur_ailes = self.createInputField("Épaisseur des ailes (mm):", 50)
 
-        self.input_forms_frame.setLayout(layout)
-
-    def createInputField(self, label_text, layout):
+        self.input_forms_frame.update()
+        
+    def createInputField(self, label_text, default_value):
         """Crée un champ d'entrée avec un label et l'ajoute à la mise en page."""
         hbox = QHBoxLayout()
         label = QLabel(label_text)
         line_edit = QLineEdit(self)
+        line_edit.setText(f"{default_value}")
+
         hbox.addWidget(label)
         hbox.addWidget(line_edit)
-        layout.addLayout(hbox)
+        self.input_forms_frame.layout().addLayout(hbox)
         return line_edit
 
     def clearLayout(self, layout):
@@ -250,7 +285,10 @@ class ShearCalculator(QWidget):
 
     def updateLayout(self):
         """Met à jour les champs d'entrée lorsque le profilé change."""
+        self.profile_type = self.profile_combobox.currentText()
         self.createProfileInputs()
+        self.scroll_area.update()
+        self.update()
         
     # Activer/désactiver la saisie du moment quadratique selon la case à cocher
     def toggle_moment_input(self, state):
@@ -264,54 +302,137 @@ class ShearCalculator(QWidget):
             self.entry_stiffness_center.setDisabled(True)
         else:
             self.entry_stiffness_center.setDisabled(False)
-            
-    def update_input_fields(self):
-        """Mettre à jour les champs d'entrée en fonction du profil sélectionné."""
-        selected_profile = self.profile_combobox.currentText()
-        
-            
+               
     # Fonction de calcul de la section d'acier
     def calculs_cisaillement_points_critiques(self):
         try:
-            # Récupérer les entrées utilisateur
-            h = float(self.entry_hauteur.text()) / 1000  # hauteur poutre en m
-            b = float(self.entry_largeur.text()) / 1000  # largeur poutre en m
+            h = 0
+            b = 0
+            h_ame = 0
+            b_ame = 0
+            h_aile = 0
+            b_aile = 0
             
-            if self.auto_moment_checkbox.isChecked():
-                I_z = (b * h**3) / 12  # Calcul du moment quadratique (m^4)
-                self.entry_quadratic_moment.setText(f"{I_z*10**12:.0f}")
-            else:
-                I_z = float(self.entry_quadratic_moment.text())  # Moment quadratique en mm4
-                I_z = I_z * 10**-12  # Convertir en m^4
+            # Récupérer les entrées utilisateur
+            if self.profile_type == PROFILE_RECT:
+                h = float(self.entry_hauteur.text()) / 1000  # hauteur poutre en m
+                b = float(self.entry_largeur.text()) / 1000 # largeur poutre en m
+        
+            elif self.profile_type == PROFILE_H:
+                h = float(self.entry_hauteur.text()) / 1000
+                b = float(self.entry_largeur.text()) / 1000
+                h_ame = float(self.entry_hauteur_ame.text()) / 1000
+                b_ame = float(self.entry_epaisseur_ame.text()) / 1000
+                b_aile = float(self.entry_largeur_ailes.text()) / 1000
+                h_aile = float(self.entry_epaisseur_ailes.text()) / 1000
+    
+            elif self.profile_type == PROFILE_T:
+                h = float(self.entry_hauteur.text()) / 1000
+                b = float(self.entry_largeur.text()) / 1000
+                h_ame = float(self.entry_hauteur_ame.text()) / 1000
+                b_ame = float(self.entry_epaisseur_ame.text()) / 1000
+                b_aile = float(self.entry_largeur_ailes.text()) / 1000
+                h_aile = float(self.entry_epaisseur_ailes.text()) / 1000                
                 
+            # Calcul de l'effort tranchant
             q = float(self.entry_charge.text()) * 1000 # charge linéaire en N/m
             longueur = float(self.entry_portee.text())  # en m
-            V = q*longueur/2 # Effort tranchant en N
+            V = q*longueur/2 # Effort tranchant en N aux points critiques sur la poutre (aux appuis)
             
+            # Calcul du centre de rigidité 
             if self.auto_stiffness_center_checkbox.isChecked():
-                y_g = h/2  # Centre d'inertie en mm
+                y_g = self.calcul_Y_g(h,b,h_ame,b_ame,h_aile,b_aile)  # Centre d'inertie en mm
                 self.entry_stiffness_center.setText(f"{y_g * 1000:.2f}")
             else:
                 y_g = int(self.entry_stiffness_center.text())  # position y du centre de rigidité en mm
                 y_g = y_g / 1000  # Convertir en m^4
                 
-            distribution_cisaillement = self.distribution_cisaillement(h, V, I_z, b, b)
-            self.display_graph(distribution_cisaillement["y"], distribution_cisaillement["tau"])
-            t_max = self.calcul_cisaillement(V,I_z,b,b,h/2-y_g,h/2)
-            self.result_label.setText(f"Contrainte de cisaillement max Txy = {t_max:.2f} MPa")
+            # Calcul du moment quadratique
+            if self.auto_moment_checkbox.isChecked():
+                I_z = self.calcul_I_z(y_g,h,b,h_ame,b_ame,h_aile,b_aile) # Calcul du moment quadratique (m^4)
+                self.entry_quadratic_moment.setText(f"{I_z*10**12:.0f}")
+            else:
+                I_z = float(self.entry_quadratic_moment.text())  # Moment quadratique en mm4
+                I_z = I_z * 10**-12  # Convertir en m^4
+        
+                
+            y, tau = self.distribution_cisaillement(V, I_z, y_g, h,b,h_ame,b_ame,h_aile,b_aile)
+            self.display_graph(y, tau)
+            #t_max = self.calcul_cisaillement(V,I_z,b,b,h/2-y_g,h/2)
+            #self.result_label.setText(f"Contrainte de cisaillement max Txy = {t_max:.2f} MPa")
         except ValueError:
             QMessageBox.critical(self, "Erreur", "Veuillez entrer des valeurs numériques valides.")
     
+    def calcul_Y_g(self,h,b,h_ame,b_ame,h_aile,b_aile):
+        if self.profile_type == PROFILE_RECT:
+            return h/2 
+        elif self.profile_type == PROFILE_H:
+            return (h_ame * b_ame * (h_ame/2 + h_aile)+ h_aile * b_aile * (h_aile / 2 + h_aile/2 + h_aile + h_ame)) / (h_ame * b_ame + h_aile * b_aile *2)
+        elif self.profile_type == PROFILE_T:
+            return (h_ame * b_ame * h_ame/2 + h_aile * b_aile * (h_aile/2 + h_ame)) / (h_ame * b_ame + h_aile * b_aile )
+        
+    def calcul_I_z(self,y_g,h,b,h_ame,b_ame,h_aile,b_aile):
+        """Calcul de moment quadratique pour une differents types de section (rectangulaire, type H et T)"""
+        if self.profile_type == PROFILE_RECT:
+            return self.I_z_rect(h,b)
+        elif self.profile_type == PROFILE_H:
+            return (self.I_z_rect(h_aile,b_aile) + b_aile * h_aile * (h_aile - y_g)**2)*2 + self.I_z_rect(h_ame,b_ame) + h_ame * b_ame * (h_ame /2 - y_g)**2     
+        elif self.profile_type == PROFILE_T:
+            return self.I_z_rect(h_aile,b_aile) + b_aile * h_aile * (h_aile - y_g)**2 + self.I_z_rect(h_ame,b_ame) + h_ame * b_ame * (h_ame /2 - y_g)**2
+        
+    def I_z_rect(self,h,b):
+        """Calcul de moment quadratique pour une section rectangulaire"""
+        return b*h**3/12
     
-    def calcul_cisaillement(self,V,I_z,b_u,b_y,y0,ymax ):
+    def calculer_cisaillement(self,V,I_z,b_u,b_y,y0,ymax ):
         """Calcul cisaillement considérant que la largeur b ne change pas sur un dy donnée"""
         integral = b_y * (ymax**2/2-y0**2/2)
         if I_z == 0 or b_y == 0:
             QMessageBox.critical(self, "Erreur", "Veuillez entrer des valeurs numériques valides : I_z ne peut pas être égal à 0 et b non plus")
             return None
         return -V/(I_z*b_u)*integral / 10**6 # contrainte de cisaillement en MPa
+            
+    def distribution_cisaillement(self,V, I_z, y_g, h,b,h_ame,b_ame,h_aile,b_aile):
+        # Calcul de la contrainte de cisaillement à différentes hauteurs
+        y_vals = np.linspace(h/2, -h/2, 1000)  # Discrétisation de la hauteur
+        cisaillements_tau = np.zeros_like(y_vals)
+        if self.profile_type == PROFILE_T:
+            for i, y in enumerate(y_vals):
+                if y > h/2 - h_aile:
+                    cisaillements_tau[i] = self.calculer_cisaillement(V,I_z,b_aile,b_aile,y,h/2)
+                else:
+                    cisaillements_tau[i] = self.calculer_cisaillement(V,I_z,b_ame,b_aile,y,h/2)
+        elif self.profile_type == PROFILE_RECT:
+            cisaillements_tau = [self.calculer_cisaillement(V,I_z,b,b,y,h/2) for y in y_vals]
+        elif self.profile_type == PROFILE_H:
+            for i, y in enumerate(y_vals):
+                if y > h/2 - h_aile or y < -h/2 + h_aile:
+                    cisaillements_tau[i] = self.calculer_cisaillement(V,I_z,b_aile,b_aile,y,h/2)
+                else:
+                    cisaillements_tau[i] = self.calculer_cisaillement(V,I_z,b_ame,b_aile,y,h/2)
+        return (y_vals, cisaillements_tau)
     
+        """def distribution_cisaillement(self,y,V,I_z,b_u,b_y,y_max):
+        "Distribution du cisaillement pour une largeur constante."
+        tau = np.zeros_like(y)
+        
+        for i, y_val in enumerate(y):
+            # Cisaillement dans l'âme
+            tau[i] = self.calcul_cisaillement(V,I_z,b_u,b_y,y_val,y_max)
+                
+        return tau"""
     
+        # Tracé du graphique
+        """plt.figure(figsize=(8, 6))
+        plt.plot(tau, y, label='Contrainte de cisaillement')
+        plt.axhline(0, color='gray', linewidth=0.5)
+        plt.xlabel('Contrainte de cisaillement (MPa)')
+        plt.ylabel('Hauteur dans la section (mm)')
+        plt.title('Distribution de la contrainte de cisaillement dans une section en H')
+        plt.legend()
+        plt.grid(True)
+        plt.show()"""
+        
     def display_graph(self,y,tau):
         try:
             # Nettoyer la figure actuelle et dessiner le graphique
@@ -328,31 +449,6 @@ class ShearCalculator(QWidget):
             self.canvas.draw()
         except ValueError:
             QMessageBox.critical(self, "Erreur", "Problèmes lors de la mise en page")
-            
-    def distribution_cisaillement(self,h,V,I_z,b_u,b_y):
-        "Distribution du cisaillement sur pour une largeur constante."
-        # Calcul de la contrainte de cisaillement à différentes hauteurs
-        y = np.linspace(h/2, -h/2, 1000)  # Discrétisation de la hauteur
-        tau = np.zeros_like(y)
-        
-        for i, y_val in enumerate(y):
-            # Cisaillement dans l'âme
-            if -h/2 <= y_val <= h/2 :
-                tau[i] = self.calcul_cisaillement(V,I_z,b_u,b_y,y_val,h/2)
-                
-        return {"y":y,"tau":tau}
-    
-        # Tracé du graphique
-        """plt.figure(figsize=(8, 6))
-        plt.plot(tau, y, label='Contrainte de cisaillement')
-        plt.axhline(0, color='gray', linewidth=0.5)
-        plt.xlabel('Contrainte de cisaillement (MPa)')
-        plt.ylabel('Hauteur dans la section (mm)')
-        plt.title('Distribution de la contrainte de cisaillement dans une section en H')
-        plt.legend()
-        plt.grid(True)
-        plt.show()"""
-        
 # Lancer l'application
 if __name__ == '__main__':
     app = QApplication(sys.argv)
